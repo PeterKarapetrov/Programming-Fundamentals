71fc258fbf4231670b7d4093df6f2fa994264c7c
Peter Karapetrov <peter.karapetrov@gmail.com>
2020-10-07 13:57:14 +0300

    Add exercises code
    Add exercises code

---
+++ Files And Exceptions/InsertLineNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace InsertLineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputText = File.ReadAllLines("input.txt").ToList();
+            for (int i = 0; i < inputText.Count; i++)
+            {
+                inputText[i] = inputText[i].Insert(0, $"{i + 1}. ");
+            }
+            File.Delete("output.txt");
+            File.WriteAllLines("output.txt", inputText.ToArray());
+        }
+    }
+}
---
+++ Files And Exceptions/OddLines/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace OddLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] textLines = File.ReadAllLines("lines.txt");
+            List<string> oddLines = new List<string>();
+            //oddLines = textLines.Where((line, index) => index % 2 == 1).ToList();
+            for (int i = 0; i < textLines.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    oddLines.Add(textLines[i]);
+                }
+            }
+            File.WriteAllLines("odd-lines.txt", oddLines.ToArray());
+        }
+    }
+}
